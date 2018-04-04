61fff729e564cbcd41a1ff82531f2de0cce0b1bc
ryuseikurata <amnosmusic13@gmail.com>
2018-04-04 14:58:33 +0900

    登録とメールアドレス認証のやつ

---
+++ register/resources/views/emails/verification.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+<div class=”container”>
+<div class=”row”>
+<div class=”col-md-8 col-md-offset-2">
+<div class=”panel panel-default”>
+<div class=”panel-heading”>Registration</div>
+<div class=”panel-body”>
+You have successfully registered. An email is sent to you for verification.
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
+
+@component('mail::message')
+# Introduction
+
+The body of your message.
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+
+@component('mail::button', ['url' => 'http://127.0.0.1:8000/verifyemail/$email_token'])
+    Verify
+@endcomponent
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
