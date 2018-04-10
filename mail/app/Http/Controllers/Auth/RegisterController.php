dcfb4f8a52e4e748c9464582f66a64ff308c9682
ryuseikurata <amnosmusic13@gmail.com>
2018-04-10 12:18:26 +0900

    やはり、メール認証わからずじまい、、、今日でQRとAPI終わらせてやります。。

---
+++ mail/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use DB;
+use Mail;
+use Illuminate\Http\Request;
+use App\Mail\EmailVerification;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'email_token' => str_random(10),
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+      //Validation
+      $validator = $this->validator($request->all());
+      if ($validator->falls())
+      {
+        $this->thorowValidationException($request, $validator);
+      }
+
+      //DBトランザクションを利用する
+      DB::beginTransaction();
+      try {
+
+      $user = $this->create($request->all());
+
+      //名前とトークンはメールビューで利用しているのでパラメーターで渡す
+      $email = new EmailVerification(new User(['name' => $user->name, 'email_token' => $user->email_token]));
+      Mail::to($user->email)->send($email);
+      DB::commit();
+
+    $request->session()->flash('message', 'にゅう直視たメールアドレスあてに「メールアドレスの登録確認」メールが届くので、確認してね');
+      return redirect('login');
+      }
+      catch (\Exception $e) {
+        //失敗したらロールバック
+        DB::rollback();
+
+        $request->session()->flash('message', 'エラー！');
+        return redirect('login');
+      }
+    }
+    public function verify($token)
+    {
+      User::where('email_token', $token)->firstOrFail()->verified();
+      request()->session()->flash('message', 'メールアドレスの登録確認が終わったよ!');
+      return redirect('login');
+    }
+}
---
+++ mail/app/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','email_token','verified'
+    ];
+
+     public function verified()
+     {
+       $this->email_token = null;
+       $this->verified = 1;
+       $this->status = 1;
+       $this->save();
+     }
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
