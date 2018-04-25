a86171a9410c9b9f7a076f754883d552dc8c897a
山田仁太 <jintayamada@yamadahitoshifutoshinoMacBook-Pro.local>
2018-04-25 23:47:16 +0900

    “dasiiodj”

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>VirtualPay</title>
+    <title>Virtual Pay</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    VirtualPay
+                    Virtual&nbsp;&nbsp;&nbsp;&nbsp;Pay
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
