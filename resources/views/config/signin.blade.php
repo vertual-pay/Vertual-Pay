bb70e23faaccdf4bdc76bd3e2dd8624dde7b6a2c
山田仁太 <jintayamada@yamadahitoshifutoshinoMacBook-Pro.local>
2018-05-08 20:20:31 +0900

    gkegiuewiilu

--- resources/views/config/signin.blade.php
+++ resources/views/config/signin.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="justify-content-center">
+    <div class="row justify-content-center">
 
-  <div class="card-header--">{{ __('設定ログイン') }}</div>
+  <div class="card-header--" style="margin-left:50px;">{{ __('設定ログイン') }}</div>
   @if(count($errors) >0)
   <div class="alert alert-danger">
   @foreach($errors->all() as $error)
@@ -15,7 +15,7 @@
   @if(empty($exist))
   <a href="/config/signup"><b style="font-size:26px; color: red;">XEMアドレスが登録されておりません。設定ページでXEMアドレスを登録してください</b></a>
   @endif
-  <form action="{{ route('config.signin') }}" method="post" style="margin-top: 50px;">
+  <form action="{{ route('config.signin') }}" method="post" style="margin-top: 100px; margin-left: -550px;">
     {{ csrf_field() }}
   <div class="form-group row">
   <label for="config_password" class="addressxem">設定ページ用パスワード</label>
